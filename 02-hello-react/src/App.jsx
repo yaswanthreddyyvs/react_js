ab792c6670848230bbc7a4cf5a7f275af3901309
Yaswanth <yvsyaswanthreddy@gmail.com>
2026-03-09 22:50:09 +0530

    day_one

---
+++ 02-hello-react/src/App.jsx
@@ -0,0 +1,9 @@
+// rafce
+
+const App = () => {
+  return (
+    <div class="yashi" >Hello yashiiiiiiiiii</div>
+  )
+}
+
+export default App
---
+++ first_folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id="dad">
+      <h1 id="child1">Hello</h1>
+      <h2 id="child2">Hello</h2>
+    </div>
+    <div id="chacha"></div>
+    </>
+  )
+}
+
+export default App
